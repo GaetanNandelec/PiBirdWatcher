9e216920c7fce4f885575aba947e79b4835ef773
Gaetan Nandelec <gaetan.nandelec@gmail.com>
2022-01-19 21:30:16 +0000

    Added function to initialise env variables

--- pibirdwatcher.py
+++ pibirdwatcher.py
@@ -10,6 +10,8 @@ import time
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 # Telegram settings
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
